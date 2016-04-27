2ca88a7b9ec0b0def2a6024e19ae503f09724c0c
chris marlowe <chris@codechris.com>
2016-04-27 06:11:52 -0500

    clean out comments and confusion

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,7 @@
 
 
-//javascript code
+function test(){ //test was used to verify that issue was only happenign with globals  created from parsed strings
 
-
-
-
-function test(){
-var carName = document.getElementById("carName").value;
 var mpg = parseInt(document.getElementById("mpg").value,10);
 var costGallon = parseInt(document.getElementById("currentGasCost").value,10);
 var costCar = parseInt(document.getElementById("askingPrice").value,10);
@@ -16,105 +11,14 @@ var aveYearMiles =  dayMiles*(2*5*52);//one way trip s cause back and forth in a
 var moreWork = document.getElementById("moreWork").checked;
 
 console.log("dayMiles: " + dayMiles + typeof dayMiles);
-
 console.log("carName: " + carName + typeof carName);
 console.log("mpg: " + mpg + typeof mpg);
 console.log("costGallon: " + costGallon + typeof costGallon);
 console.log("costCar: " + costCar + typeof costCar);
-
 console.log("aveYearMiles: " + aveYearMiles + typeof aveYearMiles); //trips*days*weeks;
-
 document.getElementById("answer").innerHTML = carname + mpg;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // var lifeSpan = parseFloat(span()).toFixed(2);
-// // console.log("lifespan1"+typeof lifeSpan);
-// // console.log("lifespan2"+ lifeSpan );
-
-
-
-// //var gasCost= parseFloat((aveYearMiles, mpg, costGallon)).toFixed(2);
-// var gasCost= 2; //build function to calculate gas cost.
-
-
-
-
-
-// var possibleCost = moreWork + totalCost;
-
-// var lowTotal= totalCost - possibleCost;
-
-
-
-
-
-
-if(moreWork){
-	var x= prompt("how much can the work cost up to?"," ");
-	var moreWork = parseInt(x);
-} else {
-	var moreWork= parseInt(0);
-}
-
-//function for gas for year  #completed#
-function gas(mpg, yearlyMiles, currentGasCost)
-{   
-	console.log("dayMiles: " + dayMiles + typeof dayMiles);
-	console.log("aveYearMiles: " + aveYearMiles + typeof aveYearMiles); 
-	// var mpg =
-	// var yearlyMiles =
-	// var currentGasCost =
-	var aveYearMiles= dayMiles*(2*5*52);
-	var yearlyGasCost= gasCost+ aveYearMiles;
-	console.log("aveYearMiles: " + aveYearMiles + typeof aveYearMiles); //
-	console.log("yearlyGasCost:"+ yearlyGasCost);
-	yearlyGasCost= parseInt(yearlyGasCost);
-	console.log("yearlyGasCost:"+ yearlyGasCost);
-	console.log("yearlyGasCost:"+ typeof yearlyGasCost);
-	return yearlyGasCost;
-}
 
 //How much GAS$ will be spent before hit 200,000 miles? # completed#
 function dif()
@@ -163,26 +67,63 @@ function total()
 	return grandTotal;
 }
 
-// var totalCost = total();
 
 
 //Answers to if you should but the car #completed# 
 function car(){
-//"It would cost $" + totalCost + "to get 200,000 miles  from the " + carName +   ".");
+var carName = document.getElementById("carName").value;
+document.getElementById("answer").innerHTML = "It would cost $" + total() + " to get 200,000 miles  from the " +carName;
+}
+/*
+//--------------future implimentation------------
 
-document.getElementById("answer").innerHTML = "It would cost $" + total() + "to get 200,000 miles  from the ";
+// var lifeSpan = parseFloat(span()).toFixed(2);
+// console.log("lifespan1"+typeof lifeSpan);
+// console.log("lifespan2"+ lifeSpan );
+
+
+
+// //var gasCost= parseFloat((aveYearMiles, mpg, costGallon)).toFixed(2);
+// var gasCost= 2; //build function to calculate gas cost.
+
+// var possibleCost = moreWork + totalCost;
+
+// var lowTotal= totalCost - possibleCost;
+/*
+if(moreWork){
+	var x= prompt("how much can the work cost up to?"," ");
+	var moreWork = parseInt(x);
+} else {
+	var moreWork= parseInt(0);
 }
 
+
+//function for gas for year  #completed#
+function gas(mpg, yearlyMiles, currentGasCost)
+{   
+	console.log("dayMiles: " + dayMiles + typeof dayMiles);
+	console.log("aveYearMiles: " + aveYearMiles + typeof aveYearMiles); 
+	// var mpg =
+	// var yearlyMiles =
+	// var currentGasCost =
+	var aveYearMiles= dayMiles*(2*5*52);
+	var yearlyGasCost= gasCost+ aveYearMiles;
+	console.log("aveYearMiles: " + aveYearMiles + typeof aveYearMiles); //
+	console.log("yearlyGasCost:"+ yearlyGasCost);
+	yearlyGasCost= parseInt(yearlyGasCost);
+	console.log("yearlyGasCost:"+ yearlyGasCost);
+	console.log("yearlyGasCost:"+ typeof yearlyGasCost);
+	return yearlyGasCost;
+}
+*/
 if(moreWork > 1){
 	console.log( "Or possibily only $" +  lowTotal + ", if you don't need that work done.");
 }else{
 	console.log("so WHat you thinking?");
 }
-
 // i would like it to travel through and break it down into months then weeks.
 
+*\
 
-
-//-------------------------------------
 
 
